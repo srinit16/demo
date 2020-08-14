6143c1b5200b3f3557c14a463888565198524ff6
srinit16 <srinit16@gmail.com>
2020-08-14 15:59:46 -0400

    main

---
+++ SpringHelloWorld/src/main/java/cogent/example/SpringHelloWorld.java
@@ -0,0 +1,14 @@
+package cogent.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorld {
+
+	public static void main(String[] args) {
+		// aa
+		SpringApplication.run(SpringHelloWorld.class, args);
+	}
+
+}
