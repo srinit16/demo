fffa6c7f6b8b308102279861fc1c898875581307
srinit16 <srinit16@gmail.com>
2020-08-14 15:43:27 -0400

    maven

--- SpringHelloWorld/src/test/java/cogent/example/SpringHelloWorldTests.java
+++ SpringHelloWorld/src/test/java/cogent/example/SpringHelloWorldTests.java
@@ -1,13 +1,10 @@
 package cogent.example;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringHelloWorldTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
